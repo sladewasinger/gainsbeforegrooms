57a0a7f187269ebc5d5ef495822f12992957d0a2
Austin Wasinger <austinwasinger@gmail.com>
2024-09-16 15:58:35 -0600

    Updated redirectUri

--- app/src/services/authService.js
+++ app/src/services/authService.js
@@ -6,7 +6,7 @@ const msalConfig = {
     clientId: "74c731c2-c972-4e07-ba9b-45271e49b0d6",  // Replace with your Azure B2C Client ID
     // authority: "https://login.microsoftonline.com/tfp/791879ec-061d-4037-81ba-51be25f70731/B2C_1_GBG_Google",
     authority: "https://gainsbeforegrooms.b2clogin.com/tfp/791879ec-061d-4037-81ba-51be25f70731/B2C_1_GBG_Google",
-    redirectUri: "http://localhost:8080/#/",  // Replace with your app's redirect URI
+    //redirectUri: "http://localhost:8080/#/",  // Replace with your app's redirect URI
   },
   cache: {
     cacheLocation: "sessionStorage", // Use localStorage to cache tokens
